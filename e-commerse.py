9983bae1d5c4bad07e682ed830d52c049ac184d7
sameer achara <sameerachara894@gmail.com>
2026-05-22 15:09:49 +0530

    Add e-commerce data analysis script
    This script performs data cleaning and analysis on an e-commerce sales dataset, including revenue calculations, product sales statistics, and a dashboard summary.

---
+++ e-commerse.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import emoji
+import math
+df = pd.read_excel("C:\\Users\\DELL\\Downloads\\ecommerce_sales_dataset.xlsx")
+
+#Data Cleaning:-
+
+print(df.isnull().sum())
+df["Price"] = pd.to_numeric(df["Price"], errors="coerce")
+df["Price"].fillna(df["Price"].median(), inplace=True)
+df["Quantity"].fillna(df["Quantity"].median(),inplace=True)
+df["City"]=df["City"].fillna("Unknown")
+df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
+df["Date"] = df["Date"].fillna(method="bfill")
+df.loc[df["Quantity"]<0, "Quantity"] = df["Quantity"].median()
+df.drop_duplicates(inplace=True)
+
+
+#Functionalities:-
+
+
+df["revenue"]=df["Price"]*df["Quantity"]
+revenue=df.groupby("Product")["revenue"].sum()
+top_revenue_product=revenue.idxmax()
+low_revenue_product=revenue.idxmin()
+print("\nProducts Revenue :\n")
+print(revenue)
+print("\nHighest Revenue Product:\n")
+print(top_revenue_product)
+print("\nLowest Revenue Product:\n")
+print(low_revenue_product)
+print("\nTotal Selled Products:\n ")
+sell=df.groupby("Product")["Quantity"].sum()
+print(sell)
+print("\ncity-wise orders / quantity:\n ")
+print(sell.idxmax())
+city_order=df.groupby("City")["Quantity"].sum()
+print(city_order)
+print("\nCity-wise sales:\n")
+city_sell=df.groupby("City")["revenue"].sum()
+print(city_sell)
+
+# Category-wise Sales trend
+print("\nTotal Sell By Each Category:\n")
+cate = df.groupby("Category")["Quantity"].sum()
+print(cate)
+print("\nTop Selling Category: \n")
+top_cate=cate.idxmax()
+print(top_cate)
+print("\nLowest Selling Category: \n")
+low_cate=cate.idxmin()
+print(low_cate)
+
+#nAOV = Total Revenue / Total Orders
+print("------AOV(Avg. Order value)------\n")
+total_revenue=df["revenue"].sum()
+total_unique_order=df["OrderID"].nunique() # yha se unique ordwer d dekh li mene
+aov=total_revenue/total_unique_order
+print(f"\nAverage ek order pe customer {aov} spend kar raha hai.\n")
+
+# Dashboard:-
+total_products_sold=df["Quantity"].sum()
+total_order=df["OrderID"].nunique()
+print("------DASHBOARD------\n")
+print(f"\nTotal-Revenue is: {total_revenue}")
+print(f"Total Orders: {total_order}")
+print(f"Total Sold Product is: {total_products_sold}\n")
+print(f"Top Selling Product is: {sell.idxmax()}")
+print(f"Lowest Selling Product is: {sell.idxmin()}\n")
+print(f"Highest Revenue Product is: {top_revenue_product}")
+print(f"Lowest Revenue Product is: {low_revenue_product}\n")
+print(f"Top City With Most Revenue is: {city_sell.idxmax()}")
+print(f"Top City With lowest revenue is: {city_sell.idxmin()}\n")
+print(f"Top City With Most Demand is: {city_order.idxmax()}")
+print(f"Top City With lowest Demand is: {city_order.idxmin()}")
+
+
+
+
+
+
+
+
+
+
+
+
